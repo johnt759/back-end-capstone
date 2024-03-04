c07106ea447433a0b016dcce1b9118aa3fb921ed
johnt759 <johntu759@gmail.com>
2024-03-04 10:28:10 -0800

    Implemented authentication methods

--- README.md
+++ README.md
@@ -6,5 +6,5 @@
 2. Phase 2 (Static content): Completed
 3. Phase 3 (Models and stored procedures): Completed
 4. Phase 4 (Adding APIs): Completed
-5. Phase 5 (User authentication): In Progress
+5. Phase 5 (User authentication): Completed
 6. Phase 6 (Testing the API): In Progress
--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -1,26 +1,29 @@
 from django.shortcuts import render
-from rest_framework import generics, viewsets, permissions
-#from rest_framework.decorators import APIView
+from rest_framework import generics, viewsets
 from .models import *
 from .serializers import *
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated] 
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated] 
